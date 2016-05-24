bc21335a7d098aba6658d58e68eed636ae2832e2
Nick Tustison <ntustison@gmail.com>
2016-05-23 19:47:21 -0700

    ENH:  promotion materials.

---
+++ faculty/promotion/README.R
@@ -0,0 +1,38 @@
+library( rmarkdown )
+library( ggplot2 )
+library( RISmed )
+
+stitchedFile <- "stitched.Rmd";
+
+formatFile <- "format.Rmd"
+
+rmdFiles <- c( formatFile, "personalStatement.Rmd"
+   )
+
+for( i in 1:length( rmdFiles ) )
+  {
+  if( i == 1 )
+    {
+    cmd <- paste( "cat", rmdFiles[i], ">", stitchedFile )
+    } else {
+    cmd <- paste( "cat", rmdFiles[i], ">>", stitchedFile )
+    }
+  system( cmd )
+  }
+
+cat( '\n Pandoc rendering', stitchedFile, '\n' )
+render( stitchedFile, output_format = "all" )
+
+# soloFiles <- c( "narrative.Rmd",
+#                  "summary.Rmd",
+#                  "introductionToRevisedApplication.Rmd"
+#               )
+# for( i in 1:length( soloFiles ) )
+#   {
+#   soloStitchedFile <- sub( '.Rmd', 'X.Rmd', soloFiles[i] )
+#   cmd <- paste( "cat", formatFile, soloFiles[i], ">", soloStitchedFile )
+#   system( cmd )
+#   render( soloStitchedFile, output_format = "pdf_document" )
+#   cat( '\n Pandoc rendering', soloFiles[i], '\n\n' )
+#   }
+
